2cbf7351b552722ca2c26630527116b23972f42c
yunusoktay <ynsokty.3@gmail.com>
2022-03-25 10:13:39 +0300

    add CartVC and cartBtn fixed

--- Eticaret/Base/BaseViewController.swift
+++ Eticaret/Base/BaseViewController.swift
@@ -22,11 +22,20 @@ class BaseViewController: UIViewController {
         navigationItem.backBarButtonItem = backBtn
         navigationItem.backBarButtonItem?.tintColor = .black
         
-       
+        let cartBtn = UIBarButtonItem(image: UIImage(named: "basket"), style: .plain, target: self, action: #selector(openBasketVC))
+        navigationItem.rightBarButtonItem = cartBtn
+        let apperance = UINavigationBarAppearance()
+        apperance.backgroundColor = UIColor(named: "lightGreen")
         
+        navigationController?.navigationBar.standardAppearance = apperance
+        navigationController?.navigationBar.compactAppearance = apperance
+        navigationController?.navigationBar.scrollEdgeAppearance = apperance
         
     }
     
-    
+    @objc func openBasketVC() {
+        let cartVC = CartVC()
+        self.navigationController?.pushViewController(cartVC, animated: true)
+    }
 
 }
